5c2d0d03a3330575630370636d0306a8c3eb0de5
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-14 19:56:46 -0300

    añado tp3

---
+++ Tp3.py
@@ -0,0 +1,11 @@
+def factorial (n):
+
+    numeros_primos = []
+    
+    for i in range (2, n + 1):
+        
+        while n % i == 0:
+            numeros_primos.append(i)
+            n = n / i
+    
+    return numeros_primos
