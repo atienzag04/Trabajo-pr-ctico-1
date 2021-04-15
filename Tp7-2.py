7831e0d0f2f4189f55ff0a785c33cd92bd7dd5f7
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-14 21:53:05 -0300

    Add files via upload

--- Tp7-2.py
+++ Tp7-2.py
@@ -1,9 +1,9 @@
 def cuantos_dias_tiene_el_mes (mes):
-    if mes in ("enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"):
+    if mes.lower() in ("enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"):
         return "31"
-    elif mes == "febrero":
+    elif mes.lower() == "febrero":
         return "28/29"
-    elif mes in ("abril", "junio", "septiembre", "noviembre"):
+    elif mes.lower() in ("abril", "junio", "septiembre", "noviembre"):
         return "30"
     else:
         return "No ingreso un mes"
