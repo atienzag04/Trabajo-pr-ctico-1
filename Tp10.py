ef81c378815399a5ad144edc0b76e37b6361e1fe
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-22 22:10:48 -0300

    añado ej 10

---
+++ Tp10.py
@@ -0,0 +1,21 @@
+import random
+
+def encontra_el_numero():
+    numero = (random.randrange(1, 20))
+    while numero < 20:
+        numero_a_adivinar = int(input("ingrese el numero: "))
+        if numero_a_adivinar < numero:
+            print ("el numero es muy bajo")
+        elif numero_a_adivinar > numero:
+            print ("el numero es muy alto")
+        else: 
+            return ("adivino el numero, felicidades!!!!")
+            
+    
+            
+                
+
+
+
+
+print(encontra_el_numero())
