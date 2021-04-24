04bca634f32038300adf3e18055919f0b72021c4
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-24 10:02:14 -0300

    añado todo el 18

--- Tp18_1.py
+++ Tp18_1.py
@@ -1,10 +1,9 @@
-def devolver_lista_alreves():
+def devolver_lista_con_auxiliar():
     lista = []
     lista_g = int(input("Enter number of elements : "))
     for i in range(0, lista_g):
-     ele = int(input())
-     lista.append(ele) # adding the element
-    lista.reverse()
-
-    return lista
-print(devolver_lista_alreves())
+     ele = (input())
+     lista.append(ele)
+    lista_auxiliar = tuple(reversed(lista))
+    return lista_auxiliar
+print(devolver_lista_con_auxiliar())
---
+++ Tp18_2.py
@@ -0,0 +1,9 @@
+def devolver_lista_alreves():
+    lista = []
+    lista_g = int(input("Enter number of elements : "))
+    for i in range(0, lista_g):
+     ele = (input())
+     lista.append(ele) # adding the element
+    lista.reverse()
+    return lista
+print(devolver_lista_alreves())
