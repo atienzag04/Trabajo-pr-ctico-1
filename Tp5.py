106d58d78f7cf9342617fd43f329d1aeab39b0b6
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 16:31:57 -0300

    Update Tp5.py

--- Tp5.py
+++ Tp5.py
@@ -1,11 +1,17 @@
+'''Quinta punto'''
+print("Tabla de comparacion farenheit-celsius")
 def defahacel():
+    #Creo las listas
     farenheit = []
     celsius = []
     i = 0
+    #Creo un bucle que va a agregar números de 0 a 120 a farenheit
     while i <= 120:
         farenheit.append(i)
         i = i + 10
     i = 0
+    #creo un bucle que usalos numeros en farenheit por órden
+    # guardandolos en celsuis en su respectivo lugar
     while i <= 12:
         numcelsius = (farenheit[i] - 32) / (9 / 5)
         celsius.append(numcelsius)
