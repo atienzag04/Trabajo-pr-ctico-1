f07e3b2053a5f9922932c75949a9509b2fa973b7
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 16:52:06 -0300

    Update Tp6.py

--- Tp6.py
+++ Tp6.py
@@ -1,13 +1,18 @@
+'''Sexto punto'''
+print ("Calculo el maximo producto")
 def mayor_producto_de_cuatro (n1, n2, n3, n4):
+    #Creo las variables a utilizar
     m1 = n1 * n2
     m2 = n1 * n3
     m3 = n1 * n4
     m4 = n2 * n3 
     m5 = n2 * n4
     m6 = n3 * n4
-    
+    #Considero a m1 como el mayor, para que luego pueda descubrir 
+    # cual es el mayor en el for
     mayor = m1
     for i in (m2, m3, m4, m5, m6):
+        #Comparo a todos las multiplicaciones, hasta encontrar la mayor
         if i > mayor:
             mayor = i
     return mayor
