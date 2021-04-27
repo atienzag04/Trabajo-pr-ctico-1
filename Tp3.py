7a775d969cfca5626e1b2b2e2add42a69d136445
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 00:07:47 -0300

    Update Tp3.py

--- Tp3.py
+++ Tp3.py
@@ -1,11 +1,11 @@
+'''Tercer punto'''
+print ("Calcular el factorial de un numero")
 def factorial (n):
-
-    numeros_primos = []
-    
+    #Creo la lista a usar
+    numeros_primos = []    
     for i in range (2, n + 1):
-        
         while n % i == 0:
             numeros_primos.append(i)
             n = n / i
-    
     return numeros_primos
+print (factorial(8))
