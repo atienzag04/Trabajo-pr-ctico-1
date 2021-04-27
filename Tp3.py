81daaded26d62cd5540b39aed17b3d930f47739a
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 16:18:51 -0300

    Update Tp3.py

--- Tp3.py
+++ Tp3.py
@@ -2,10 +2,15 @@
 print ("Calcular el factorial de un numero")
 def factorial (n):
     #Creo la lista a usar
-    numeros_primos = []    
+    numeros_primos = []  
+    #Creo un loop, para que divida por todos los numeros hasta llegar a n
     for i in range (2, n + 1):
+        #Esto es, para que al encontrar el divisor, se agregue el numero a 
+        #mi lista, y a n lo divide por i para que ahora empieze a factoriza 
+        # al número resultante
         while n % i == 0:
             numeros_primos.append(i)
             n = n / i
+    #Devuelvo el resultado
     return numeros_primos
 print (factorial(8))
