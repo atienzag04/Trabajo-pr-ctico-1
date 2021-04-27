ab8c289e926364fe91caa737e5f056b8b690929c
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 00:01:28 -0300

    Delete Tp1.py

--- Tp1.py
+++
@@ -1,5 +0,0 @@
-nombre = input ("ingrese el nombre: ")
-print( nombre + ", buen dia." )
-numero1 = int (input ("ingrese el primer numero: "))
-numero2 = int (input ("ingrese el segundo numero: "))
-print (numero1 * numero2)
