50d10985dfc0daf30394cec8dc450736c535845d
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 16:27:28 -0300

    Update Tp4.py

--- Tp4.py
+++ Tp4.py
@@ -1,2 +1,10 @@
-def fahrenheit_a_celsius(grados_fahrenheit):
-    return (grados_fahrenheit - 32) * (5/9)
+'''Cuarta punto'''
+print ("Pasar grados de celsius a fahrenheit")
+def fahrenheit_a_celsius():
+    #Pido los grados en fahrenheit
+    grados_fahrenheit = int(input("Ingrese los grados en Fahrenheit: "))
+    #Calculo el valor en Celsius
+    celsius = (grados_fahrenheit - 32) * (5/9)
+    #Muestro el resultado
+    return celsius
+print (fahrenheit_a_celsius())
