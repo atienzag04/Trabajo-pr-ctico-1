e104cbca041e015c6a03c212a8eebdef1554436e
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 00:01:13 -0300

    añado ambas partes del primer punto, completo

---
+++ Tp1_1.py
@@ -0,0 +1,9 @@
+'''Primer punto'''
+'''Primera parte'''
+print ("Saludo al usuario")
+#Creo la funcion
+def saludar_al_usuario():
+    #Pido al usuario que ingrese los datos a utilizar
+    nombre = input ("ingrese el nombre: ")
+    #Devuelvo el saludo
+    return( nombre + ", buen dia." )
