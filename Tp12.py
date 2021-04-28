360d47f018a12b9c354e11807679e02237c7c0aa
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 23:59:48 -0300

    Update Tp12.py

--- Tp12.py
+++ Tp12.py
@@ -1,11 +1,16 @@
-def contar_vocales(palabra):
+'''Decimo segundo punto'''
+
+def contar_vocales():
+    #Creo las variables para contar las variables
     contador_a = 0
     contador_e = 0
     contador_i = 0
     contador_o = 0
     contador_u = 0
     total=0
+    palabra = str(input("Ingrese la palabra a la que desea contar las vocales: "))
     for letra in palabra:
+        #Busco las letras en las palabras, y las sumo en su contador
         if letra.lower() in "a":
             contador_a += 1
         if letra.lower() in "e":
@@ -16,8 +21,5 @@ def contar_vocales(palabra):
             contador_o += 1
         if letra.lower() in "u":
             contador_u += 1
-            
+
     return contador_a, contador_e, contador_i, contador_o, contador_u
-
-
-print (contar_vocales("holla"))
