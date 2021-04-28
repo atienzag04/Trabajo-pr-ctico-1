bf466fe5825d9c0f2a172c852529732d5ca959fc
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 23:03:38 -0300

    Update Tp8.py

--- Tp8.py
+++ Tp8.py
@@ -1,18 +1,17 @@
-def conversion_entero_romano(entero):
+'''Octavo punto'''
+print ("Pasar numeros enteros a romanos")
+def conversion_entero_romano():
+    #Creo las listas para almacenar los numeros
     numeros = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     numerales = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
-
     numeral = ''
     i = 0
-
+    entero = int(input("Ingrese el año a pasar a romanos: "))
+    #Calculo un while para calcular los numeros en romano
     while entero > 0:
-        for _ in range(entero // numeros[i]):
+        for fechas in range(entero // numeros[i]):
             numeral += numerales[i]
             entero -= numeros[i]
-
         i += 1
-    
     return numeral
-
-
-print(conversion_entero_romano(1)) 
+print(conversion_entero_romano()) 
