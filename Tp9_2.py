c231e7b3b4925324d4b147e20d6285f334633d81
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 23:50:18 -0300

    Update Tp9_2.py

--- Tp9_2.py
+++ Tp9_2.py
@@ -1,20 +1,23 @@
+'''Noveno punto'''
+'''Segunda parte'''
+
 import time
 
 def sacar_la_contraseña():
+    #Ingreso cual es la contraseña
     contasena = "admin"
     intentos = 1
+    #Creo el while para asegurar que el numero de intenos que le quedan 
+    #no es 6
     while intentos != 6:
         contrasena_de_usuario = (input("ingrese la contraseña: "))
         if contrasena_de_usuario == contasena:
             print ("felicidades, ingreso la conraseña correcta")
         else:
             print ("no esta ingresando la contraseña correcta ")
+            #uso el time.sleep para hacer que cada vez pase mas tiempo entre
+            #cada intento
             intentos = intentos + 1
-            time.sleep (1 * intentos)
+            (time.sleep (1 * intentos))
             if intentos == 6:
-                print ("No tiene mas intentos")
-
-
-            
-        
-print (sacar_la_contraseña())
+                return ("No tiene mas intentos")
