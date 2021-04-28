89ec482a791085f81cba189f4c1793800c0f324a
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 23:50:49 -0300

    Update Tp9_3.py

--- Tp9_3.py
+++ Tp9_3.py
@@ -1,6 +1,11 @@
+'''Noveno punto'''
+'''Tercera parte'''
+
 def sacar_la_contraseña():
+    #Ingreso cual es la contraseña
     contasena = "admin"
     contrasena_de_usuario = (input("ingrese la contraseña: "))
+    #Utilizo el if, y la varieble bool para mostrar cual es la respuesta
     if contrasena_de_usuario == contasena:
         return "felicidades, ingreso la conraseña correcta"
         correcto = True
@@ -11,8 +16,3 @@ def sacar_la_contraseña():
         return "felicidades, ingreso la conraseña correcta"
     elif correcto == False:
         return "no esta ingresando la contraseña correcta "
-
-
-
-
-print (sacar_la_contraseña())
