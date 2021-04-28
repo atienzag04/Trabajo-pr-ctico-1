1dc06d1c28ce5911279aa695061200c2236d9fc8
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-27 23:48:32 -0300

    Update Tp9_0.py

--- Tp9_0.py
+++ Tp9_0.py
@@ -1,10 +1,12 @@
+'''Noveno punto'''
+'''Parte 0'''
+
 def sacar_la_contraseña():
+    #Ingreso cual es la contraseña
     contasena = "admin"
     contrasena_de_usuario = (input("ingrese la contraseña: "))
+    #Creo un if para saber si la contraseña es correcta
     if contrasena_de_usuario == contasena:
         return "felicidades, ingreso la conraseña correcta"
     else:
         return "no esta ingresando la contraseña correcta "
-
-
-print (sacar_la_contraseña())
