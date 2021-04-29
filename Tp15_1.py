826bcac378997d5efd0337786d91c2d9814fcf27
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 22:13:16 -0300

    Update Tp15_1.py

--- Tp15_1.py
+++ Tp15_1.py
@@ -1,8 +1,13 @@
-def primer_letra_por_palabra(palabras):
+"""Decimo quinto punto"""
+
+def primer_letra_por_palabra():
+    """Muestra las palaras iniciales de la frase a ingresar"""
+    palabras = str(input("Ingrese la frase que desea mostrar las iniciales: "))
+    #Separo cada palabra en palabras_separadas
     palabras_separadas = palabras.split(' ')
     caracteres = ""
+    #Utilizo el for para que se guarde cada inicial
     for la_palabra in palabras_separadas:
         caracteres += la_palabra[0]
     return caracteres
 
-print(primer_letra_por_palabra("viva peron"))
