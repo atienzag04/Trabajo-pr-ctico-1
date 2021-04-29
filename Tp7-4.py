ee977e744756d37f721c432ba0dbb6c2fc5a2e53
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:45:57 -0300

    Update Tp7-4.py

--- Tp7-4.py
+++ Tp7-4.py
@@ -1,18 +1,18 @@
-def dias_para_terminar_mes (mes, dias):
-    
+"""Septimo punto"""
+"""Cuarta parte"""
+def cuantos_faltan_hasta_final_mes (dias_que_pasaron, mes):
+    """Se devuelve cuanto falta hasta el fin de mes"""
     if mes.lower() in ("enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"):
-        dias_total = 31
+        dias_del_mes = 31
+        dias_que_pasaron = dias_del_mes - dias_que_pasaron 
+        return dias_que_pasaron
     elif mes.lower() == "febrero":
-        dias_total = 28/29
+        dias_del_mes = 28
+        dias_que_pasaron = dias_del_mes - dias_que_pasaron 
+        return dias_que_pasaron
     elif mes.lower() in ("abril", "junio", "septiembre", "noviembre"):
-        dias_total = 30
+        dias_del_mes = 30
+        dias_que_pasaron = dias_del_mes - dias_que_pasaron 
+        return dias_que_pasaron
     else:
-        return "No ingreso una fecha valida"
-
-    if dias <= dias_total:
-        dias_que_faltan = dias_total - dias
-        return dias_que_faltan
-    else: 
-        return "No ingreso una fecha valida"
-
-print (dias_para_terminar_mes("junio", 12))
+        return "No ingreso un mes"
