9933eb77e6ba4cb09387dcbf74d4fb270a21253c
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:24:24 -0300

    Update Tp18_2.py

--- Tp18_2.py
+++ Tp18_2.py
@@ -1,9 +1,14 @@
+"""Decimo octavo punto"""
+"""Segunda parte"""
 def devolver_lista_alreves():
+    """Se devuelve una lista de elementos dada por el usuario, dada vuelta
+    sin el uso de una lista auxiliar"""
     lista = []
-    lista_g = int(input("Enter number of elements : "))
+    lista_g = int(input("Ingrese numeros de elementos: "))
     for i in range(0, lista_g):
-     ele = (input())
-     lista.append(ele) # adding the element
+        ele = (input())
+        lista.append(ele) 
+    #Se utiliza el for para saber cuantos elementos hay en la funcion
     lista.reverse()
     return lista
 print(devolver_lista_alreves())
