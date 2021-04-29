6d3703e67bf65cd21b483d79a59a767cbf12aff2
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:21:30 -0300

    Update Tp1_1.py

--- Tp1_1.py
+++ Tp1_1.py
@@ -1,8 +1,8 @@
 '''Primer punto'''
 '''Primera parte'''
-print ("Saludo al usuario")
 #Creo la funcion
 def saludar_al_usuario():
+    """Saludo al usuario"""
     #Pido al usuario que ingrese los datos a utilizar
     nombre = input ("ingrese el nombre: ")
     #Devuelvo el saludo
