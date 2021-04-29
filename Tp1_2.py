b30b6461bf3576979f90353da8e2b7f7554fcdab
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:25:57 -0300

    Update Tp1_2.py

--- Tp1_2.py
+++ Tp1_2.py
@@ -1,10 +1,11 @@
 '''Primer punto'''
 '''Segunda parte'''
-print ("Podructo de 2 numeros")
 #Creo la funcion
 def producto_dos_numeros():
+    "Podructo de 2 numeros"
     #Pido los 2 numeros a multiplicar
     numero1 = int (input ("ingrese el primer numero: "))
     numero2 = int (input ("ingrese el segundo numero: "))
     #Devuelvo la multiplicacion
-    return (numero1 * numero2)
+    multiplicacion = numero1 * numero2
+    return multiplicacion
