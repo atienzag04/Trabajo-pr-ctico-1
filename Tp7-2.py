4b040022d5217148d1912862d89d622072f3a8e4
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:31:37 -0300

    Update Tp7-2.py

--- Tp7-2.py
+++ Tp7-2.py
@@ -1,4 +1,7 @@
+"""Septimo punto"""
+"""Segunda parte"""
 def cuantos_dias_tiene_el_mes (mes):
+    """Se devuelve la cantidad de dias dependiendo del mes"""
     if mes.lower() in ("enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"):
         return "31"
     elif mes.lower() == "febrero":
