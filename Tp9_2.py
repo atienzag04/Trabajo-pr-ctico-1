945c858b6d238c47424c52a0299cffc3f8d76330
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:52:00 -0300

    Update Tp9_2.py

--- Tp9_2.py
+++ Tp9_2.py
@@ -1,23 +1,19 @@
 '''Noveno punto'''
-'''Segunda parte'''
-
-import time
+'''Tercera parte'''
 
 def sacar_la_contraseña():
+    """Programa para descubrir la contraseña, utilizando una variable bool"""
     #Ingreso cual es la contraseña
     contasena = "admin"
-    intentos = 1
-    #Creo el while para asegurar que el numero de intenos que le quedan 
-    #no es 6
-    while intentos != 6:
-        contrasena_de_usuario = (input("ingrese la contraseña: "))
-        if contrasena_de_usuario == contasena:
-            print ("felicidades, ingreso la conraseña correcta")
-        else:
-            print ("no esta ingresando la contraseña correcta ")
-            #uso el time.sleep para hacer que cada vez pase mas tiempo entre
-            #cada intento
-            intentos = intentos + 1
-            (time.sleep (1 * intentos))
-            if intentos == 6:
-                return ("No tiene mas intentos")
+    contrasena_de_usuario = (input("ingrese la contraseña: "))
+    #Utilizo el if, y la varieble bool para mostrar cual es la respuesta
+    if contrasena_de_usuario == contasena:
+        return "felicidades, ingreso la conraseña correcta"
+        correcto = True
+    else:
+        correcto = False
+
+    if correcto == True:
+        return "felicidades, ingreso la conraseña correcta"
+    elif correcto == False:
+        return "no esta ingresando la contraseña correcta "
