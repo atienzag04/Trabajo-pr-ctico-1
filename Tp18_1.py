2e50f141bdeab41ec95442dfe721316a30fcfd50
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:23:00 -0300

    Update Tp18_1.py

--- Tp18_1.py
+++ Tp18_1.py
@@ -1,9 +1,14 @@
+"""Decimo octavo punto"""
+"""Primera parte"""
 def devolver_lista_con_auxiliar():
+    """Se devuelve una lista de elementos dada por el usuario, dada vuelta"""
+    #Se utiliza una lista auxiliar, para guardar la nueva lista con los 
+    # elementosdados vuelta
     lista = []
-    lista_g = int(input("Enter number of elements : "))
+    lista_g = int(input("Ingrese el numero de elementos : "))
+    #Se utiliza el for para saber cuantos elementos hay en la funcion
     for i in range(0, lista_g):
      ele = (input())
      lista.append(ele)
     lista_auxiliar = tuple(reversed(lista))
     return lista_auxiliar
-print(devolver_lista_con_auxiliar())
