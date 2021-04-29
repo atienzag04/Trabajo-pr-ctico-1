1ca10c14dc96fee160383425add10514475917ed
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:13:56 -0300

    Update Tp16_1.py

--- Tp16_1.py
+++ Tp16_1.py
@@ -1,8 +1,10 @@
-def removerVocales(palabra):
+"""Decimo sexto punto"""
+"""Primera parte"""
+def remover_vocales():
+  """Se retiran las vocales de la palabra"""
+  palabra = str(input("Ingrese una palabra para retirarle las vocales: "))
   letras = []
   for caracter in palabra:
     if caracter.lower() not in 'aeiou':
       letras.append(caracter)
   return ''.join(letras) 
-
-print (removerVocales('Diego'))
