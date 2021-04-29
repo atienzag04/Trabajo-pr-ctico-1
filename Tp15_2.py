dbf5716dee88187c7709f67e9e00b9fe14044683
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 22:35:35 -0300

    Update Tp15_2.py

--- Tp15_2.py
+++ Tp15_2.py
@@ -1,3 +1,7 @@
-def pasar_primera_letra_mayuscula(palabra):
-    return (palabra.title())
-print(pasar_primera_letra_mayuscula("hola"))
+"""Decimo quinto punto"""
+"""Segunda parte"""
+def pasar_primera_letra_mayuscula():
+    """Cambio la palabra dada para cambiar el primer caracter mayusculas"""
+    palabra = str(input("Ingrese la palabra para cambiarle la primera letra: "))
+    palabra = palabra.title()
+    return palabra
