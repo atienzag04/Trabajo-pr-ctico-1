e8d1205dd63f7387a0d9727f7af91dcbdc1ebb02
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:32:01 -0300

    Update Tp4.py

--- Tp4.py
+++ Tp4.py
@@ -1,6 +1,6 @@
 '''Cuarta punto'''
-print ("Pasar grados de celsius a fahrenheit")
 def fahrenheit_a_celsius():
+    """Pasar grados de celsius a fahrenheit"""
     #Pido los grados en fahrenheit
     grados_fahrenheit = int(input("Ingrese los grados en Fahrenheit: "))
     #Calculo el valor en Celsius
