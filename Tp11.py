3603c09cc02486d2eb2f038d5479397a90e3dbad
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:12:38 -0300

    Update Tp11.py

--- Tp11.py
+++ Tp11.py
@@ -1,7 +1,15 @@
-def revision_de_examen(numero_de_ejercicios, porcentaje):
+'''Undécima Parte'''
+
+def revision_de_examen():
+    """Calculo de apobados o desaprobados"""
     valor_centinela = ""
     alumno = 0
+    #Pido las variables al usuario
+    numero_de_ejercicios = int(input("Ingrese el numero de ejercicios: "))
+    porcentaje = int(input("Ingrese el porcentaje por punto: "))
     cantidad_porcentaje = (numero_de_ejercicios * porcentaje)//100
+    #Creo el while, para saber si aprobo o no el alumno
+    #El sistema no para hasta que se escriba ok
     while valor_centinela != "ok":
         cantidad_ejercicios =  int(input("Ingrese la cantidad de ejercicios "))
         if cantidad_ejercicios >= cantidad_porcentaje:
@@ -9,6 +17,8 @@ def revision_de_examen(numero_de_ejercicios, porcentaje):
         else: 
             resultado = "desaprobo"
         alumno += 1
+        #Muestro el resultado
         print ("El alumno " + str(alumno) + " " + resultado)
         valor_centinela = str(input("Ingrese ok si desea terminar el proceso "))
     return alumno
+print (revision_de_examen())
