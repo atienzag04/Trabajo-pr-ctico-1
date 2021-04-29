75b8c525214ebdcdec937bf7a7e4270f70f6d0c1
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:09:43 -0300

    Update Tp16_4.py

--- Tp16_4.py
+++ Tp16_4.py
@@ -1,4 +1,11 @@
-def encontrar_palindrome(palabra):
+"""Decimo sexto punto"""
+"""Cuarta parte"""
+def encontrar_palindrome():
+    """Se verifica si una palabra es palindrome"""
+    palabra = str(input("Ingrese la palabra para averiguar si una palabra es palindrome: "))
+    #Utilizo el lower y el replace para que las palabras 
+    #se sepan mejor y sin complicaciones
+    palabra = palabra.lower()
     palabra = palabra.replace(" ","")
     verificador = (palabra == palabra[::-1])
     return verificador
