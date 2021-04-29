1d6594e16398e489329b8a2e814fe5a6469cb72b
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 21:59:13 -0300

    Update Tp13.py

--- Tp13.py
+++ Tp13.py
@@ -1,8 +1,13 @@
-def iguales_pero_mayusculas (primerapalabra, segundapalabra):
+"""Decimo tercer punto"""
+
+def iguales_pero_mayusculas ():
+    primera_palabra = str(input("Ingrese la primera palabra a comparar: "))
+    segunda_palabra = str(input("Ingrese la segunda palabra a comparar: "))
     Resultado= bool
-    if primerapalabra == segundapalabra.upper():
+    #Se compara la primera palabra con la segunda
+    if primera_palabra == segunda_palabra.upper():
         Resultado = True
     else:
         Resultado = False
     return Resultado 
-print (iguales_pero_mayusculas("HOLA", "hola"))
+print (iguales_pero_mayusculas())
