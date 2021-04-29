1feed05cfe9abe433efcbacfdcc542d86fadc647
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 23:16:24 -0300

    Update Tp16_2.py

--- Tp16_2.py
+++ Tp16_2.py
@@ -1,8 +1,10 @@
-def removerConsonantes(palabra):
+"""Decimo sexto punto"""
+"""Segunda parte"""
+def remover_consonantes():
+  """Se retiran las consonantes de la palabra"""
+  palabra = str(input("Ingrese una palabra para retirarle las consonantes: "))
   letras = []
   for caracter in palabra:
-    if caracter.lower() not in 'bdfghjklmnñpqrstvxyz':
+    if caracter.lower() in 'aeiou':
       letras.append(caracter)
   return ''.join(letras) 
-
-print (removerConsonantes('Diego'))
