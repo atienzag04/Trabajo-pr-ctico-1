dcb7654181f7f317bfbf9aa94f3fe8dd7e89b14b
atienzag04 <80432205+atienzag04@users.noreply.github.com>
2021-04-28 22:40:21 -0300

    Update Tp15_3.py

--- Tp15_3.py
+++ Tp15_3.py
@@ -1,12 +1,12 @@
-
-
-def mostrar_palabras_comienzen_con_a(palabras):
-  palabras_separadas = palabras.split(' ')
-  la_a = "a"
-  for i in palabras_separadas:
-    if str(palabras_separadas) in la_a:
-      ases = palabras_separadas[i]
-    else:
-      ases = ""
-  return ases
-print(mostrar_palabras_comienzen_con_a("aloja soy"))
+"""Decimo quinto punto"""
+"""Tercera parte"""
+def palabras_con_a():
+  """Cada palabra que comience con a se muestra"""
+  palabra = str(input("Ingrese un texto: ")).split(' ')
+  resultado = ' '
+  #Uso el for para que cada palabra que comience con A se muestre 
+  for i in range(0, len(palabra)):
+    cadena = palabra[i]
+    if cadena[0] in ["A", "a"]:
+      resultado += palabra[i] + " "
+  return resultado
